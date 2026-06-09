1ad8d423585847f40d3736dbac00fe81d8af9250
Yusef Codes <youcefcopy@gmail.com>
2026-06-09 15:39:54 +0100

    exercice 3 implementation

--- src/EmetteurAgent.java
+++ src/EmetteurAgent.java
@@ -1,31 +1,56 @@
 import jade.core.Agent;
 import jade.core.AID;
 import jade.core.behaviours.TickerBehaviour;
+import jade.domain.DFService;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.domain.FIPAException;
 import jade.lang.acl.ACLMessage;
 
 public class EmetteurAgent extends Agent {
     private static final long serialVersionUID = 1L;
-    int counter = 0;
-
 
     protected void setup() {
         addBehaviour(new TickerBehaviour(this, 5000) {
+            int counter = 0;
+
             protected void onTick() {
-                ACLMessage msg = new ACLMessage(ACLMessage.INFORM);
-                msg.addReceiver(new AID("Alice", AID.ISLOCALNAME));
-                if (counter == 1) {
-                    msg.setContent("Quitter");
-                    send(msg);
-                    System.out.println("Message Quitter envoye. Bob se supprime aussi.");
-                    stop();        // ← stops the TickerBehaviour
-                    doDelete();    // ← kills Bob agent
-                    return;
-                } else {
+                // Search DF for "Imprimante" service
+                DFAgentDescription template = new DFAgentDescription();
+                ServiceDescription sd = new ServiceDescription();
+                sd.setType("Imprimante");
+                template.addServices(sd);
+
+                try {
+                    DFAgentDescription[] results = DFService.search(myAgent, template);
+
+                    if (results.length == 0) {
+                        System.out.println("Aucun agent Imprimante trouve dans le DF.");
+                        return;
+                    }
+
+                    AID imprimante = results[0].getName();
+
+                    ACLMessage msg = new ACLMessage(ACLMessage.INFORM);
+                    msg.addReceiver(imprimante);
+
+                    if (counter == 1) {
+                        msg.setContent("Quitter");
+                        send(msg);
+                        System.out.println("Message Quitter envoye. Bob se supprime aussi.");
+                        stop();
+                        doDelete();
+                        return;
+                    }
+
                     msg.setContent("Hello, es-tu la ?");
                     counter++;
+                    send(msg);
+                    System.out.println("Message envoye a " + imprimante.getLocalName() + ". (tick " + counter + ")");
+
+                } catch (FIPAException e) {
+                    e.printStackTrace();
                 }
-                send(msg);
-                System.out.println("Message envoye a Alice.");
             }
         });
     }
--- src/RecepteurAgent.java
+++ src/RecepteurAgent.java
@@ -1,5 +1,9 @@
 import jade.core.Agent;
 import jade.core.behaviours.CyclicBehaviour;
+import jade.domain.DFService;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.domain.FIPAException;
 import jade.lang.acl.ACLMessage;
 
 public class RecepteurAgent extends Agent {
@@ -8,13 +12,26 @@ public class RecepteurAgent extends Agent {
     protected void setup() {
         System.out.println("Agent " + getLocalName() + " est pret.");
 
+        // Register "Imprimante" service in the DF
+        DFAgentDescription dfd = new DFAgentDescription();
+        dfd.setName(getAID());
+        ServiceDescription sd = new ServiceDescription();
+        sd.setType("Imprimante");
+        sd.setName("service-impression");
+        dfd.addServices(sd);
+        try {
+            DFService.register(this, dfd);
+            System.out.println("Alice enregistree dans le DF comme Imprimante.");
+        } catch (FIPAException e) {
+            e.printStackTrace();
+        }
+
         addBehaviour(new CyclicBehaviour() {
             public void action() {
                 ACLMessage msg = receive();
                 if (msg != null) {
                     System.out.println("Message recu de " + msg.getSender().getLocalName() + " : " + msg.getContent());
 
-                    // ...
                     if (msg.getContent().equals("Quitter")) {
                         System.out.println("Message Quitter recu. Alice se supprime.");
                         doDelete();
@@ -30,5 +47,14 @@ public class RecepteurAgent extends Agent {
                 }
             }
         });
+    }
+
+    protected void takeDown() {
+        try {
+            DFService.deregister(this);
+            System.out.println("Alice desenregistree du DF.");
+        } catch (FIPAException e) {
+            e.printStackTrace();
+        }
     }
 }
